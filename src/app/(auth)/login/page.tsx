88e6bd58c28fece32daa57e3f9214a36a76cb01c
myabdi4 <myabdi4@gmail.com>
2026-06-09 16:43:44 -0600

    Fix build config

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -77,7 +77,7 @@ export default function LoginPage() {
           </button>
 
           <p className="text-center text-sm text-gray-500">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link
               href="/signup"
               className="text-blue-900 font-medium hover:underline"
